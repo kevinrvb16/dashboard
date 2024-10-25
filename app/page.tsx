9fe0c5a551c75fe498ca06a9abf1f6f8154e32d7
kevinrvb16 <kevinrvb16@gmail.com>
2024-10-24 21:57:27 -0300

    Tela de login

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
 
 export default function Home() {
   return (
-    <main>Olá mundo</main>
+    <main>
+      <div className="h-screen flex justify-center items-center bg-slate-600 px-5">
+        <form className="bg-white p-12 rounded-lg w-96 max-w-full flex justify-center items-center flex-col gap-2">
+          <h2 className="font-bold text-lg mb-6">Faça o login</h2>
+          <input type="email" placeholder="Email" className="input input-primary w-full" />
+          <input type="password" placeholder="Senha" className=" input input-primary w-full" />
+          <button className="btn btn-primary w-full">Login</button>
+        </form>
+      </div>
+    </main>
   );
 }
