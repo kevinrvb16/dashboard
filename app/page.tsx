ea5d4e4cbc24e81e4974e29b285a0bae4fe079fa
Kevin Rafael Velez Bernal <kevin.velez@ahgora.com.br>
2024-10-25 22:17:35 -0300

    suspense ajustado

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import LoginForm from "@/components/LoginForm"
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main>
       <div className="h-screen flex justify-center items-center bg-slate-600 px-5">
+      <Suspense fallback={<div className="flex justify-center items-center">Carregando...</div>}>
         <LoginForm />
+      </Suspense>
       </div>
     </main>
   );
